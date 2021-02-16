53cc2d3dc794b4c3349d598e66f08a8b77513d1e
Miguelpenha <miguel.penha1@gmail.com>
2021-02-16 16:46:01 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import styles from './components/styles.module.css'
 
 export default function Home() {
+    if (typeof window !== 'undefined') {
+        var form = document.getElementById('formulario');
+        var campo = document.getElementById('campo');
+
+        form.addEventListener('submit', function(e) {
+            window.location.replace('https://usando-api-do-github-filipe-deschamps.vercel.app/contas/'+campo.value)
+            e.preventDefault();
+        });
+    }
     return (
         <>
             <head>
@@ -8,6 +17,10 @@ export default function Home() {
             </head>
             <main className={styles.main}>
                 <h1>Home</h1>
+                <form id="formulario">
+                    <input type="text" id="campo"></input>
+                    <button type="submit">Enviar</button>
+                </form>
             </main>
         </>
     )
