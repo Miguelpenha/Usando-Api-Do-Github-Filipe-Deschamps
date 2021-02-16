29b137356b3be22e7a8131eb67aab61df3c8e1e9
Miguelpenha <miguel.penha1@gmail.com>
2021-02-16 15:06:38 -0300

    Update [nome].js

--- pages/contas/[nome].js
+++ pages/contas/[nome].js
@@ -14,6 +14,10 @@ export async function getStaticProps(context) {
       `https://api.github.com/users/${contaGithub}/repos`
     );
     const dadosReposJson = await dadosRepos.json()
+    const dadosP = await fetch(
+        `https://usando-api-do-github-filipe-deschamps.vercel.app/contasApi/${contaGithub}`
+      );
+    const dadosPJson = await dadosP.json()
     var nome = ''
     if (dadosJson.name === null) {
         nome = 'nulo'
@@ -24,7 +28,8 @@ export async function getStaticProps(context) {
         props: {
             seguidores: await dadosJson.followers,
             repositórios: await dadosJson.public_repos,
-            nome: nome
+            nome: nome,
+            dadoP: dadosPJson.nome
         }
     }
 }
@@ -34,6 +39,7 @@ export default function Contas(props) {
             <h1>Nome Do Usuário: {props.nome}</h1>
             <h1>Número De Seguidores: {props.seguidores}</h1>
             <h1>Número De Repositórios: {props.repositórios}</h1>
+            <h1>dadoP: {props.dadoP}</h1>
         </main>
     )
 }
