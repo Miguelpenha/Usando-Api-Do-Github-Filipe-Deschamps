c9967b6ec5b27760cee5ec2362bde02ad1d5e08b
Miguelpenha <miguel.penha1@gmail.com>
2021-02-18 16:33:44 -0300

    Update [nome].js

--- pages/contas/[nome].js
+++ pages/contas/[nome].js
@@ -31,7 +31,7 @@ export async function getStaticProps(context) {
     var quant_orgs = dadosOrgsJson.length
     var organizações = ''
     while (quant_orgs > 0) {
-        if (quant_orgs === 1) {
+        if (quant_orgs === 0) {
             organizações = `${organizações}${dadosOrgsJson[quant_orgs-1].name}`
         }else {
             organizações = `${organizações}${dadosOrgsJson[quant_orgs-1].name}, `
