bdfbe949244e7acd91bc0b813b1f4ca063a7e716
Miguelpenha <miguel.penha1@gmail.com>
2021-02-22 15:20:54 -0300

    Update teste.module.js

--- pages/components/teste.module.js
+++ pages/components/teste.module.js
@@ -1,8 +1,8 @@
 export default function Teste(props) {
     var organizações = String(props.organizações).split(',')
     return (
-        <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
+        <h1>
             {organizações}
-        </a>
+        </h1>
     )
 }
