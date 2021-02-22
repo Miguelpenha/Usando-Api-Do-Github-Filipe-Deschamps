180fba42f889ccac8877ba22abfdd95a15b0acf1
Miguelpenha <miguel.penha1@gmail.com>
2021-02-22 15:27:04 -0300

    Update teste.module.js

--- pages/components/teste.module.js
+++ pages/components/teste.module.js
@@ -2,8 +2,8 @@ import styles from '../components/styles.module.css'
 
 export default function Teste(props) {
     var organizações = String(props.organizações).split(',')
-    organizações.forEach((org) => {
-        return (
+    return organizações.forEach((org) => {
+        (
             <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
                 {org}
             </a>
