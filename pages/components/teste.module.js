c6e3f977e3e116d20d172277f62142f5270f0fce
Miguelpenha <miguel.penha1@gmail.com>
2021-02-22 15:15:27 -0300

    Update teste.module.js

--- pages/components/teste.module.js
+++ pages/components/teste.module.js
@@ -1,9 +1,3 @@
 export default function Teste(props) {
-    return props.organizações.forEach((org) => {
-        return (
-            <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'+org} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
-                {org}
-            </a>
-        )
-    })
+    return <h1>{props.organizações}</h1>
 }
