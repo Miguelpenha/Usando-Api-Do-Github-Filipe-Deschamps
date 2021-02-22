b2dbdecb0b3de2c2d09ca899fab9c9e7708f72ed
Miguelpenha <miguel.penha1@gmail.com>
2021-02-22 15:23:51 -0300

    Update teste.module.js

--- pages/components/teste.module.js
+++ pages/components/teste.module.js
@@ -1,8 +1,12 @@
+import styles from '../components/styles.module.css'
+
 export default function Teste(props) {
     var organizações = String(props.organizações).split(',')
-    return (
-        <h1>
-            {organizações}
-        </h1>
-    )
+    while (organizações) {
+        return (
+            <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
+                asd
+            </a>
+        )
+    }
 }
