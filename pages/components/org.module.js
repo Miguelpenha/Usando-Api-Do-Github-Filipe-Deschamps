70f629387e999a0919b02be1299c783377de750a
Miguelpenha <miguel.penha1@gmail.com>
2021-02-22 15:55:29 -0300

    Update org.module.js

--- pages/components/org.module.js
+++ pages/components/org.module.js
@@ -2,7 +2,7 @@ import styles from '../components/styles.module.css'
 
 export default function Org(props) {
     return (
-        <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'+props.org} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
+        <a className={styles.link_conta} href={'https://github.com/'+props.nick+'/'+String(props.org).replace(',')} target="_blank" rel="external" className={styles.link_conte} style={{textDecoration: 'none'}}>
             {props.org}
         </a>
     )
